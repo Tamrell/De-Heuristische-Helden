9f613430c51a06a1bf7b5f97fa7f4f2c744d77f6
Joeri Fresen <joerifresen@gmail.com>
2018-03-08 20:55:05 +0100

    added plot of heatmap

--- grid.py
+++ grid.py
@@ -1,11 +1,15 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly.offline import plot
+
 class Grid:
 
     def __init__(self, houses):
         self.grid_list = []
         self.total_distance = 0
         self.total_probability = 0
-        for x in range(50):
-            for y in range(50):
+        for y in range(50):
+            for x in range(50):
                 self.grid_list.append(Grid_Point(x, y, houses))
                 for house in houses:
                     self.total_distance += abs(house["x"] - x)
@@ -14,6 +18,14 @@ class Grid:
             self.total_probability += (self.total_distance / point.distance)
         for point in self.grid_list:
             point.probability = (self.total_distance / point.distance) / self.total_probability
+
+    def y_list(self, y):
+        start_index = (y - 1) * 50
+        end_index = start_index + 50
+        dist_list = []
+        for i in range(start_index, end_index):
+            dist_list.append(self.grid_list[i].probability)
+        return dist_list
 
 class Grid_Point:
 
@@ -26,13 +38,18 @@ class Grid_Point:
         self.y = y
 
 
+grid = Grid([{"x": 28, "y": 28}, {"x": 45, "y": 45}, {"x": 44, "y": 44}])
 
+trace = go.Heatmap(z = [grid.y_list(i) for i in range(1, 51)])
 
-grid = Grid([{"x": 48, "y": 48}, {"x": 24, "y": 24}])
-sum = 0
+data = [trace]
 
-for point in grid.grid_list:
-    sum += point.probability
+plot(data, filename='labelled-heatmap.html')
 
-print(sum)
-# print(grid.grid)
+# sum = 0
+
+# for point in grid.grid_list:
+#     sum += point.probability
+#     print(point.probability)
+
+# print(sum)
