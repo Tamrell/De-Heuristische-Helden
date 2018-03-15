99023de4c5e6532fdce0f6855116b3a3919740bd
Angelo Groot <angelogroot@hotmail.com>
2018-03-15 19:10:00 +0100

    nay

--- house.py
+++ house.py
@@ -1,4 +1,6 @@
 class House:
+    ''' House class, initializes with tuple: coordinate and int: output'''
+
     def __init__(self, (x, y), output):
         self.cord = (x, y)
         self.output = output
