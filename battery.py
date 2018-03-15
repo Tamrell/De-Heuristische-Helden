d88be31d1cdaad1f6e6064ab7e2b5f521eec9e5d
Angelo Groot <angelogroot@hotmail.com>
2018-03-15 20:18:19 +0100

    Find closest battery function added

--- battery.py
+++ battery.py
@@ -1,8 +1,8 @@
 class Battery:
     ''' Battery class, initializes with tuple: coordinate and int: max_load'''
 
-    def __init__(self, (x, y), max_load):
-        self.cord = (x, y)
+    def __init__(self, cord, max_load):
+        self.cord = cord
         self.max_load = max_load
         self.load = 0
         self.links = []
