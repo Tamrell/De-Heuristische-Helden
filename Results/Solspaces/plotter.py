0fe193523e90611e805cea1ed0204b9289d54424
Angelo Groot <angelogroot@hotmail.com>
2018-04-20 10:39:22 +0200

    added the plot function to the main menugit add .! :D

--- Results/Solspaces/plotter.py
+++ Results/Solspaces/plotter.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(color_codes=True)
 
-nbh = input("which neighbourhood do you want the distribution plot of?"\n)
-with open('SS' + nbh + '.csv') as df:
-    data = [int(line) for line in df]
-    fig = sns.distplot(data)
-    plt.show()
 
-    print('done!')
+def plotter(nbh):
+    with open('Results/Solspaces/SS' + nbh + '.csv') as df:
+        data = [int(line) for line in df]
+        fig = sns.distplot(data)
+        plt.show()
+        print('done!')
