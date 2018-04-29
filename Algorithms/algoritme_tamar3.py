77fbc356d4cffd2fad7d558494b0aa6356683a92
vist18 <tamer.vis@student.uva.nl>
2018-04-29 12:46:39 +0200

    hi

--- Algorithms/algoritme_tamar3.py
+++ Algorithms/algoritme_tamar3.py
@@ -10,91 +10,114 @@ from grid import *
 from plots import *
 import connections
 import time
+import random
 
 # get first case
 fileDir = os.path.abspath(os.path.join(__file__, '..', '..', 'Data'))
 filename1 = os.path.join(fileDir, 'wijk1_huizen.csv')
 filename2 = os.path.join(fileDir, 'wijk1_batterijen.txt')
 
-args = len(sys.argv)
-if args > 0:
-    b_sort = str(sys.argv[1])
-else:
-    b_sort = None
-if args > 1:
-    filter_stack = True if sys.argv[2] == "T" else False
+def setup():
+    args = len(sys.argv)
 
-root = Grid(filename1, filename2, "1")
+    end_condition = str(sys.argv[1])
+    if end_condition[-1] == "s":
+        def running(start_time, iterations):
+            return time.time() - start_time < int(sys.argv[1][:-1])
+    else:
+        def running(start_time, iterations):
+            return iterations < int(sys.argv[1])
 
-houses = list(root.houses)
-n_houses = len(houses)
+    sort_method = str(sys.argv[2])
+    if sort_method == "dist":
+        def battery_list(grid, house):
+            return sorted([(grid.houses[house].dists[battery], battery) for battery in grid.batteries.values()], key = operator.itemgetter(0), reverse = True)
+    elif sort_method == "load":
+        def battery_list(grid, house):
+            return sorted([(battery.load, battery) for battery in grid.batteries.values()], key = operator.itemgetter(0), reverse = False)
+    else:
+        def battery_list(grid, house):
+            return [(battery.load, battery) for battery in grid.batteries.values()]
 
-batteries = list(root.batteries)
+    if len(sys.argv) > 3:
+        filter_stack = True if str(sys.argv[3]) == "True" else False
+        shuffle_stack = True if str(sys.argv[4]) == "True" else False
 
-connections = connections.Connections()
-bound = 10000
-best_case = None
-import random
-stack = [(bound, root)]
+        if filter_stack:
+            def filter(stack, bound):
+                init_len = len(stack)
+                for grid in stack:
+                    if grid[1].score() > bound:
+                        stack.remove(grid)
+                print("filter removed\t", init_len - len(stack), "\tcases")
+                return stack
+        else:
+            def filter(stack, bound):
+                return True
 
-def filter(stack, bound):
-    init_len = len(stack)
-    for grid in stack:
-        if grid[1].score() > bound:
-            stack.remove(grid)
-    print("filter removed\t", init_len - len(stack), "\tcases")
-    return stack
+        if shuffle_stack:
+            def shuffle(stack):
+                print("Shuffelling")
+                return random.shuffle(stack)
+        else:
+            def shuffle(stack):
+                return stack
+    return (running, battery_list, filter, shuffle)
 
-def b_list(grid, house, sort):
-    if sort == "dist":
-        return sorted([(grid.houses[house].dists[battery], battery) for battery in grid.batteries.values()], key = operator.itemgetter(0), reverse = True)
-    if sort == "load":
-        return sorted([(battery.load, battery) for battery in grid.batteries.values()], key = operator.itemgetter(0), reverse = False)
-    if sort == "None":
-        return [(grid.houses[house].dists[battery], battery) for battery in grid.batteries.values()]
+def search(root, connections):
+    print("Setting up\n")
+    (running, battery_list, filter, shuffle) = setup()
 
-start_time = time.time()
-iter = 0
+    start_time, iter, count, bound, best_case = time.time(), 0, 0, 10000, None
 
-count = 0
-while True:#time.time() - start_time < 600:
-    if len(stack) == 0:
-        sys.exit()
-        #stack.sort(key=operator.itemgetter(0), reverse = True)
+    houses = list(root.houses)
+    n_houses = len(houses)
+    batteries = list(root.batteries)
 
-    score, current_grid = stack.pop()
+    stack = [(bound, root)]
 
-    if len(current_grid.houses) == 0:
-        #random.shuffle(stack)
-        score = current_grid.score()
-        if score < bound:
-            count = 0
-            bound = score
-            best_case = current_grid
+    while running(start_time, iter):
+        if len(stack) == 0:
+            sys.exit()
 
-            if filter_stack:
+        score, current_grid = stack.pop()
+
+        if len(current_grid.houses) == 0:
+            score = current_grid.score()
+            if score < bound:
+                count = 0
+                bound = score
+                best_case = current_grid
+
                 stack = filter(stack, bound)
 
-            print("solution", iter, current_grid.score())
-        continue
+                print("solution", iter, current_grid.score())
+            continue
 
-    house = list(current_grid.houses).pop()
-    battery_list = b_list(current_grid, house, b_sort)
-    for dist, battery in battery_list:
-        if connections.connect(current_grid.houses[house], battery):
-            if current_grid.score() < bound:
+        house = list(current_grid.houses).pop()
+        b_list = battery_list(current_grid, house)
+        for dist, battery in b_list:
+            if connections.connect(current_grid.houses[house], battery):
+                if current_grid.score() < bound:
 
-                new_grid = copy.deepcopy(current_grid)
-                del new_grid.houses[house]
+                    new_grid = copy.deepcopy(current_grid)
+                    del new_grid.houses[house]
 
-                stack.append((new_grid.score(), new_grid))
-                connections.unconnect(current_grid.houses[house])
+                    stack.append((new_grid.score(), new_grid))
+                    connections.unconnect(current_grid.houses[house])
 
-    if iter % 100 == 0:
-        print(iter, len(stack), bound)
-    if count == 1000:
-        random.shuffle(stack)
-        print("reset")
-        count = 0
-    iter += 1
-    count += 1
+        if iter % 10 == 0:
+            print(iter, len(stack), bound)
+        if count == 1000:
+            stack == shuffle(stack)
+            print("reset")
+            count = 0
+        iter += 1
+        count += 1
+
+if __name__ == "__main__":
+    print("Alles geladen")
+    root = Grid(filename1, filename2, "1")
+    connections = connections.Connections()
+    print("Root geinitialiseerd")
+    search(root, connections)
