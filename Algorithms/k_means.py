1fb15f4b09011b91370c6815755fe5b673cf29da
Tamrell <angelogroot@hotmail.com>
2018-05-25 20:50:03 +0200

    welp

--- Algorithms/k_means.py
+++ Algorithms/k_means.py
@@ -2,19 +2,20 @@ from Algorithms.Helpers.connect import connect
 
 def k_means(grid):
     """
-        tries to find ideal k-means configurations for all combinations of
-        4-10(?) batteries and then compares them to find the cheapest
-        and best configuration
+        Tries to find ideal k-means configuration for the grid.
 
     Takes
         Grid: grid containing the houses used for the k-means
 
     Returns
-        ?????
+        None
     """
-    for b in grid.batteries.values():
-        cluster_mean(grid, b)
-    grid.light_reset()
+    moved = True
+    while moved:
+        moved = False
+        for b in grid.batteries.values():
+            if cluster_mean(grid, b):
+                moved = True
 
 def cluster_mean(grid, bat, restricted=False):
     '''
@@ -26,21 +27,21 @@ def cluster_mean(grid, bat, restricted=False):
         Battery: battery for which the new location will be calculated
 
     Returns
-        None
+        Bool: True if the battery has been moved, else False
     '''
-    # note that this mean may already be containing a house.
     x_val = 0
     y_val = 0
-    if restricted:
-        houses = [h.cord for h in bat.links]
-    else:
-        houses = [h.cord for h in grid.houses.values() if h.closest_battery(grid) == bat]
+    houses = [h.cord for h in grid.houses.values() if h.closest_battery(grid) == bat]
     if houses:
+
+        # Calculate the mean coordinate of the cluster.
         for x, y in houses:
             x_val += x
             y_val += y
         x_val = int(x_val / len(houses))
         y_val = int(y_val / len(houses))
+
+        # Move battery to the mean, or next to it if it contains a house.
         for i in [0, 1, -1]:
             if not (x_val + i, y_val) in grid.houses:
                 grid.move_battery(bat ,(x_val + i, y_val))
