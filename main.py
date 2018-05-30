a95dda7b5714b2c04ecf8f638d42ac47556dd620
Tamrell <angelogroot@hotmail.com>
2018-05-30 16:17:18 +0200

    ay

--- main.py
+++ main.py
@@ -26,75 +26,12 @@ from tqdm import tqdm
 import sys
 import time
 
-def get_neighbourhood():
-    """
-        This function selects the neighbourhood to run algorithms for, depending
-        on user input.
-        Takes:
-            None
-        Returns:
-            The number of the neighbourhood to run the algorithms for.
-    """
-    legal = {'1', '2', '3'}
-    print("\n For which neighbourhood do you want to run an algorithm?")
-    nbh = input("\n(int): ")
-
-    # validity checking
-    while not nbh in legal:
-        print(nbh, "is an invalid choice, please choose 1, 2 or 3")
-        nbh = input("\n(int): ")
-    return nbh
-
-def get_algorithm():
-    """
-    This function selects the algorithm to run.
-
-    Takes:
-        None
-
-    Returns:
-        string: user input resembling the algorithm to run
-    """
-
-    print("\n Which algorithm do you want to run?")
-    print("\n <key>: <algorithm>\n\n s: Solspace\n p: Procrastinator")
-    print(" lower: Worst (illegal?) Config Finder")
-    print(" upper: best (illegal?) Config Finder")
-    print(" d: Shortest Depthest First")
-    print(" e: evaluator")
-    print(" r: Random Connect")
-    print(" a: A-smart")
-    print(" b: Battery Cycler")
-    print(" n: Plot the distribution of the random solution space")
-    print(" h: Print Heatmap")
-    print(" bb: Branch and bound\n")
-    return input("(alpha): ")
-
-def run_algorithm(alg, grid):
-    """
-        The function run_algorithm(alg, grid) calls one of the primary algorithms,
-        depending on the parameter alg.
-        Takes:
-            alg: string representing the algorithm to run
-            grid: grid.Grid object to hand to the selected algorithms
-        Returns:
-            return name of selected algorithm
-    """
-    algorithms = {
-                  'r': ['Random Connect', random_sampler],
-                  'b': ['battery cycler', battery_cycler],
-                  'e': ['evaluate', evaluate_distribution],
-                  'l': ['lower bound', lower_bound],
-                  'u': ['upper bound', upper_bound],
-                  }
-    return algorithms[alg][1](grid)
-    return algorithms[alg][0]
-
 def run():
     results = []
     for i in range(100):
         print(i)
         results.append(run_algorithm('b', grid))
+        #greedy_hillclimber(grid)
     return(min(results))
 
 if __name__ == "__main__":
@@ -113,3 +50,18 @@ if __name__ == "__main__":
         grid = Grid(nbh, houses, batteries)
     else:
         grid = Grid(nbh, houses)
+
+
+###############testing purposes##################
+    k_data(grid)
+    #start_simulation(grid)
+    exit()
+
+#################################################
+    """
+    plotter(nbh)
+
+    alg = get_algorithm()
+    alg = run_algorithm(alg, grid)
+    """
+# for testing purposes
